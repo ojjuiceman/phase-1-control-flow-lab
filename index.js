a73ddebbdf1b09101c5f68b214c28a050c98f326
Okera Johnson <okerajohnson@gmail.com>
2021-10-13 11:38:22 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  }
+
+  else if (feet >= 2001 && feet <= 2499) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feet >= 2501) {
+    return 'No can do.';
+  }
+    
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(bity){
+  if (bity == "NYC") {
+    return "Ok, sounds good.";
+  }
+  else if (bity != "NYC") {
+    return "No go.";
+  }
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipp){
+  if (tipp == "generous") {
+    return 'Thank you so much.';
+  }
+  else if (tipp == "not as generous") {
+    return "Thank you.";
+  }
+  else if (tipp != "generous" && tipp != "not as generous") {
+    return "Bye.";
+  }
 }
